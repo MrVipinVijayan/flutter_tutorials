cb3b39956d0bfb83bc70c7ffead93132d5fe4c7b
Vipin Vijayan <vipinvijayan23@gmail.com>
2022-08-07 01:46:13 -0500

    flutter tutorial links added

--- flutter_tutorials/lib/pages/home_page.dart
+++ flutter_tutorials/lib/pages/home_page.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+// Flutter Best Practices Tutorials can be found in the below links
+// Flutter Best Practices - Part 1
+// https://itnext.io/flutter-best-practices-part-1-e89467ea4823
+
+// Flutter Best Practices - Part 2
+// https://itnext.io/flutter-best-practices-part-2-e9e5c79ccb16
+
+// Flutter Best Practices - Part 3
+// https://itnext.io/flutter-best-practices-part-3-747f1bfaec6b
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
