5ef98898214a6f9a34714704ac259886a39bc9c3
flutterthemer <flutterthemer@gmail.com>
2023-07-23 20:10:02 -0500

    feat: android setup updated

--- flutter_tutorials/android/app/src/main/kotlin/com/example/flutter_tutorials/MainActivity.kt
+++ flutter_tutorials/android/app/src/main/kotlin/com/example/flutter_tutorials/MainActivity.kt
@@ -1,6 +1,26 @@
 package com.example.flutter_tutorials
 
+import android.content.Context
+import com.ryanheise.audioservice.AudioServicePlugin
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+
+    override fun provideFlutterEngine(context: Context): FlutterEngine? {
+        return AudioServicePlugin.getFlutterEngine(context)
+    }
+
+    override fun getCachedEngineId(): String {
+        AudioServicePlugin.getFlutterEngine(this);
+        return AudioServicePlugin.getFlutterEngineId();
+    }
+
+    override fun shouldDestroyEngineWithHost(): Boolean {
+        return false;
+    }
 }
