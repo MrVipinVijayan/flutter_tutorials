0f491252b1a3676ae3662b6695b8f05393d04e8e
flutterthemer <flutterthemer@gmail.com>
2023-12-25 13:37:17 -0600

    fix: clean up main branch

--- flutter_tutorials/lib/main.dart
+++ flutter_tutorials/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorials/pages/home_page.dart';
-import 'package:flutter_tutorials/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,23 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Theme Extensions',
-      // theme: ThemeData(
-      //   primarySwatch: Colors.blue,
-      // ),
-      // theme: ThemeData.light().copyWith(
-      //   extensions: <ThemeExtension<dynamic>>[
-      //     MyColors.light,
-      //   ],
-      // ),
-      // darkTheme: ThemeData.dark().copyWith(
-      //   extensions: <ThemeExtension<dynamic>>[
-      //     MyColors.dark,
-      //   ],
-      // ),
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      home: const HomePage(),
+      title: 'Flutter Tutorials',
+      home: Container(),
     );
   }
 }
