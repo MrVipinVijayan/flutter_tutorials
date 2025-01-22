b265d39cf2e0097d9826f26a13cc0af18e7f8c21
flutterthemer <flutterthemer@gmail.com>
2025-01-21 20:48:53 -0600

    feat: add carouselview example

--- flutter_tutorials/lib/main.dart
+++ flutter_tutorials/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorials/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Tutorials',
-      home: Container(),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
