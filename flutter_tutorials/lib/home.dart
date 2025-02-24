4b5b15bdecbbbdf8aa02a89363d574f8152528b2
flutterthemer <flutterthemer@gmail.com>
2025-02-23 21:29:17 -0600

    feat: update carousel

--- flutter_tutorials/lib/home.dart
+++ flutter_tutorials/lib/home.dart
@@ -18,53 +18,107 @@ class _HomeState extends State<Home> {
     "https://images.unsplash.com/photo-1659671026913-b7e632f17395"
   ];
 
+  double itemSize = 330;
+  double shrinkSize = 200;
+  double prevPosition = 0;
+
+  void listener() {
+    print('Changed');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    carouselController.addListener(listener);
+  }
+
+  @override
+  void dispose() {
+    carouselController.removeListener(listener);
+    super.dispose();
+  }
+
+  void moveTo(double pos) {
+    // carouselController.jumpTo(pos);
+    carouselController.animateTo(
+      pos,
+      duration: const Duration(seconds: 1),
+      curve: Curves.easeIn,
+    );
+    prevPosition = pos;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Carousel'),
       ),
-      body: ConstrainedBox(
-        constraints: const BoxConstraints(maxHeight: 200),
-        child: CarouselView(
-          controller: carouselController,
-          itemExtent: 330,
-          shrinkExtent: 200,
-          enableSplash: true,
-          scrollDirection: Axis.horizontal,
-          children: List.generate(
-            images.length,
-            (index) {
-              return Image.network(
-                images[index],
-                filterQuality: FilterQuality.high,
-                fit: BoxFit.cover,
-                loadingBuilder: (context, child, loadingProgress) {
-                  if (null == loadingProgress) {
-                    return child;
-                  }
-                  final totalBytes = loadingProgress.expectedTotalBytes ?? 1;
-                  final loadedBytes = loadingProgress.cumulativeBytesLoaded;
-                  final progress = loadedBytes / totalBytes;
+      body: Column(
+        children: [
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 200),
+            child: CarouselView(
+              controller: carouselController,
+              itemExtent: itemSize,
+              shrinkExtent: shrinkSize,
+              enableSplash: true,
+              scrollDirection: Axis.horizontal,
+              children: List.generate(
+                images.length,
+                (index) {
+                  return Image.network(
+                    images[index],
+                    filterQuality: FilterQuality.high,
+                    fit: BoxFit.cover,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (null == loadingProgress) {
+                        return child;
+                      }
+                      final totalBytes =
+                          loadingProgress.expectedTotalBytes ?? 1;
+                      final loadedBytes = loadingProgress.cumulativeBytesLoaded;
+                      final progress = loadedBytes / totalBytes;
 
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CircularProgressIndicator(value: progress),
-                      const SizedBox(height: 10),
-                      Text('${(progress * 100).toStringAsFixed(0)}%'),
-                    ],
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircularProgressIndicator(value: progress),
+                          const SizedBox(height: 10),
+                          Text('${(progress * 100).toStringAsFixed(0)}%'),
+                        ],
+                      );
+                    },
+                    errorBuilder: (context, error, stackTrace) {
+                      return const Center(
+                        child: Text('Failed to load image'),
+                      );
+                    },
                   );
                 },
-                errorBuilder: (context, error, stackTrace) {
-                  return const Center(
-                    child: Text('Failed to load image'),
-                  );
-                },
-              );
-            },
+              ),
+            ),
           ),
-        ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  if (prevPosition > 0) moveTo(prevPosition - itemSize);
+                },
+                child: const Text('Prev'),
+              ),
+              const SizedBox(width: 20),
+              ElevatedButton(
+                onPressed: () {
+                  moveTo(prevPosition + itemSize);
+                },
+                child: const Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
